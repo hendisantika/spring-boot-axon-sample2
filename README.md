fbe5b4b9f3fedb607088908d5ae79bb670560bf8
Hendi Santika <hendisantika@gmail.com>
2021-11-17 07:20:21 +0700

    #1 - Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# spring-boot-axon-sample2
---
+++ src/main/java/com/hendisantika/SpringBootAxonSample2Application.java
@@ -0,0 +1,13 @@
+package com.hendisantika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAxonSample2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAxonSample2Application.class, args);
+    }
+
+}
