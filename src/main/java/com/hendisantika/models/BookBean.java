31dbe7f089c6e85dab3f56f9d94aa235bd28ed25
Hendi Santika <hendisantika@gmail.com>
2021-11-17 07:26:51 +0700

    #9 - Add BookBean

---
+++ src/main/java/com/hendisantika/models/BookBean.java
@@ -0,0 +1,18 @@
+package com.hendisantika.models;
+
+import lombok.Data;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-axon-sample2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 17/11/21
+ * Time: 07.26
+ */
+@Data
+public class BookBean {
+    private String isbn;
+    private String title;
+}
