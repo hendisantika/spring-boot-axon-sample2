07ba076fdfa8b09dae76a23d265b37a0212a0199
Hendi Santika <hendisantika@gmail.com>
2021-11-17 07:28:21 +0700

    #12 - Add GetLibraryQuery

---
+++ src/main/java/com/hendisantika/queries/GetLibraryQuery.java
@@ -0,0 +1,17 @@
+package com.hendisantika.queries;
+
+import lombok.Data;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-axon-sample2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 17/11/21
+ * Time: 07.28
+ */
+@Data
+public class GetLibraryQuery {
+    private final Integer libraryId;
+}
