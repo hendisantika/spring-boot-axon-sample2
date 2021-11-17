97c5f50e0d02791697ebc1282e39fa778fae96ac
Hendi Santika <hendisantika@gmail.com>
2021-11-17 07:35:54 +0700

    #26 - Add getLibrary API in LibraryRestController

--- src/main/java/com/hendisantika/controller/LibraryRestController.java
+++ src/main/java/com/hendisantika/controller/LibraryRestController.java
@@ -1,14 +1,21 @@
 package com.hendisantika.controller;
 
+import com.hendisantika.aggregate.Library;
 import com.hendisantika.command.RegisterLibraryCommand;
 import com.hendisantika.models.LibraryBean;
+import com.hendisantika.queries.GetLibraryQuery;
 import lombok.RequiredArgsConstructor;
 import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.axonframework.queryhandling.QueryGateway;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 /**
  * Created by IntelliJ IDEA.
@@ -33,4 +40,9 @@ public class LibraryRestController {
         return "Saved";
     }
 
+    @GetMapping("/{library}")
+    public Library getLibrary(@PathVariable Integer library) throws InterruptedException, ExecutionException {
+        CompletableFuture<Library> future = queryGateway.query(new GetLibraryQuery(library), Library.class);
+        return future.get();
+    }
 }
