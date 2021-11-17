79f4902cba6b6d0c5596c64c98c3ec6d5f0ab4c0
Hendi Santika <hendisantika@gmail.com>
2021-11-17 07:27:22 +0700

    #10 - Add LibraryBean

---
+++ src/main/java/com/hendisantika/models/LibraryBean.java
@@ -0,0 +1,18 @@
+package com.hendisantika.models;
+
+import lombok.Data;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-axon-sample2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 17/11/21
+ * Time: 07.27
+ */
+@Data
+public class LibraryBean {
+    private int libraryId;
+    private String name;
+}
