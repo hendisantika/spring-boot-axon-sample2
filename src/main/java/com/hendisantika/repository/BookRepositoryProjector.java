c7e94713fb554262664b4ed43aaa0e328aeb3356
Hendi Santika <hendisantika@gmail.com>
2021-11-17 07:30:20 +0700

    #16 - Add addBook in BookRepositoryProjector

--- src/main/java/com/hendisantika/repository/BookRepositoryProjector.java
+++ src/main/java/com/hendisantika/repository/BookRepositoryProjector.java
@@ -1,6 +1,8 @@
 package com.hendisantika.repository;
 
+import com.hendisantika.events.BookCreatedEvent;
 import lombok.RequiredArgsConstructor;
+import org.axonframework.eventhandling.EventHandler;
 import org.springframework.stereotype.Service;
 
 /**
@@ -18,4 +20,12 @@ public class BookRepositoryProjector {
 
     private final BookRepository bookRepository;
 
+    @EventHandler
+    public void addBook(BookCreatedEvent event) throws Exception {
+        BookEntity book = new BookEntity();
+        book.setIsbn(event.getIsbn());
+        book.setLibraryId(event.getLibraryId());
+        book.setTitle(event.getTitle());
+        bookRepository.save(book);
+    }
 }
